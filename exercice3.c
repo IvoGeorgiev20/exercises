8aef3a791f0b27542f482a0ef79d64566f334146
Ivo <ivo.dragonfly@gmil.com>
2020-07-28 11:08:03 +0300

    exercices 1, 2, 3

---
+++ exercice3.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+
+    int one;
+    int two;
+    int multiply;
+
+    printf("Enter first number - ");
+    scanf("%d",&one);
+
+    printf("Enter second number - ");
+    scanf("%d",&two);
+
+    multiply = one * two;
+
+    printf("The multiplication of numbers %d and %d is %d",one,two,multiply);
+
+return 0;
+
+}
+
+
