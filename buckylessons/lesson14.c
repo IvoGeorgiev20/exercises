96bdfaf807b4a6d696fff049e675289483cc7314
Ivo <ivo.dragonfly@gmil.com>
2020-07-28 11:22:59 +0300

    fix lesson 14

--- buckylessons/lesson14.c
+++ buckylessons/lesson14.c
@@ -8,8 +8,8 @@ int main()
     printf ("Enter your age\n");
     scanf ("%f", &age3);
 
-    average = (age1 + age2 + age3) / 3;
-    printf ("The avarage age of the group is %f\n", average);
+    avarage = (age1 + age2 + age3) / 3;
+    printf ("The avarage age of the group is %f\n", avarage);
 
     return 0;
 }
