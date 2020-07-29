440267636b4eb7ffd0ada93156155b680a231908
Ivo <ivo.dragonfly@gmil.com>
2020-07-29 13:30:52 +0300

    formatting

--- buckylessons/lesson33.c
+++ buckylessons/lesson33.c
@@ -6,15 +6,21 @@ int main()
 
     if( isalpha(tuna) )
     {
-        if(isupper(tuna) ){
+        if(isupper(tuna) )
+        {
             printf("%c is an upper case letter", tuna);
         }else{
             printf("%c is an lower case letter", tuna);
         }
-    }else{
-        if(isdigit(tuna) ){
+        }
+        else
+        {
+        if(isdigit(tuna) )
+        {
             printf("%c is a number, tuna");
-        }else{
+        }
+        else
+        {
             printf("%c is a OMG WTF is sthat!?", tuna);
         }
     }
