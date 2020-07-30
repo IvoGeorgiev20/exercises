1c12cdba6f7bf99ec2ccfc69a8ed45c4c1442f92
Martin <martin@libtec.org>
2020-07-30 20:31:47 +0900

    Better code for exercise1.c

--- exercise1.c
+++ exercise1.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
-#define array 100
+#define ARR_MAX 100
 
 int main()
 {
-    int n;
-    int arr[array];
-    int i;
-    int max;
-    int min;
+    int arr[ARR_MAX];
     int sum = 0;
 
-    printf("Enter size of the array: ");
-    scanf("%d", &n);
+    int n;
+    printf ("Enter size of the array: ");
+    scanf ("%d", &n);
 
-    printf("Enter elements in the array: ");
-    for(i=0; i<n; i++)
+    printf ("Enter elements in the array: ");
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
         sum = sum + arr[i];
     }
 
-    max = arr[0];
-    min = arr[0];
+    int max = arr[0];
+    int min = arr[0];
 
-    for(i=1; i<n; i++)
+    for (int i = 1; i < n; i++)
     {
-        if(arr[i] > max)
+        if (arr[i] > max)
         {
             max = arr[i];
         }
-        if(arr[i] < min)
+        if (arr[i] < min)
         {
             min = arr[i];
         }
-
     }
+
     float mean = (float) sum / n;
     printf("mean is: %f\n", mean);
     printf("sum is: %d\n", sum);
     printf("Maximum element = %d\n", max);
-    printf("Minimum element = %d", min);
+    printf("Minimum element = %d\n", min);
 
     return 0;
 }
