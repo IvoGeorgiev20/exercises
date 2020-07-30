17aa20d1a6b89b31b67ebdba59f2b8b270c6e858
Martin <martin@libtec.org>
2020-07-30 20:48:08 +0900

    Made exercise2.c better.

--- exercise2.c
+++ exercise2.c
@@ -3,20 +3,19 @@
 int main()
 {
     int arr[100];
+
     int n;
-    int i;
+    printf ("Enter size of the array: ");
+    scanf ("%d", &n);
 
-    printf("Enter size of the array: ");
-    scanf("%d", &n);
-
-    printf("Enter elements in array: ");
-    for(i=0; i<n; i++)
+    printf ("Enter elements in array: ");
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }
 
     printf("Array in reverse order: ");
-    for(i = n-1; i>=0; i--)
+    for (int i = n-1; i >= 0; i--)
     {
         printf("%d", arr[i]);
     }
