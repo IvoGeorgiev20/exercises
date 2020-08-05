439bd95a63704c2fc45a4f647e166495b90c8153
Ivo <ivo.dragonfly@gmil.com>
2020-08-05 19:25:09 +0300

    changes

--- exercise5.c
+++ exercise5.c
@@ -1,17 +1,18 @@
-//*През конзолата се приемат едно по едно число и след това степен(няколко пъти), програмата трябва да пресметне стпойността*//
+/*През конзолата се приемат едно по едно число и след това степен(няколко пъти), програмата трябва да пресметне стпойността*/
 
 #include <stdio.h>
 
 int main()
 {
-    int n = 10;
+    int n;
+    printf("Enter the numbers: ");
+    scanf("%d", n);
     int i;
     for(i = 0; i < n; i++)
     {
         int num;
         int pow;
         int product = 1;
-        product = 1;
         int j;
         printf("\nEnter the number: ");
         scanf("%d", &num);
