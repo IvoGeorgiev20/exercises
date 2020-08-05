011169bdc322397f916d58e43c2623868176e71c
Ivo <ivo.dragonfly@gmil.com>
2020-08-05 18:51:53 +0300

    changes

--- exercise5.c
+++ exercise5.c
@@ -4,25 +4,23 @@
 
 int main()
 {
-    int num;
     int n = 10;
-    int pow;
-    int product = 1;
     int i;
-    int p;
     for(i = 0; i < n; i++)
     {
+        int num;
+        int pow;
+        int product = 1;
+        product = 1;
         printf("\nEnter the number: ");
         scanf("%d", &num);
         printf("Enter the power: ");
         scanf("%d", &pow);
-        for(p = 0; p < pow; p++)
+        for(i = 0; i < pow; i++)
         {
             product = num * product;
         }
         printf("The product is: %d\n", product);
     }
-
-
     return 0;
 }
